607551480c7351b76d5c84d97b411f7d3eee5ce6
John Costanzo <jrock2004@gmail.com>
2018-06-15 12:43:10 -0400

    Fixing how test are run

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,5 @@ fi
 #echo "Starting chrome for testing"
 xvfb-run --server-args='-screen 0, 1024x768x16' google-chrome -start-maximized > /dev/null &
 
-(ember test --silent --reporter xunit || true) > /tmp/test_results.xml
+ember test
+
